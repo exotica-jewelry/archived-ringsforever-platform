10af80808fe85bf24d734b6a02ea993f03403baa
Ivan Boothe <ivan@rootwork.org>
2016-02-05 15:32:27 -0800

    Moved from simple CSS override file to Sass/Gulp/styleguide workflow

---
+++ sites/all/themes/trf/README.md
@@ -0,0 +1,100 @@
+# The Titanium Rings Forever theme
+
+TRF is a custom Drupal theme.
+
+## Basic development instructions
+
+TRF is compiled from Sass into CSS using `gulp`. We use `gulp-sass`, which uses
+`node-sass`, which runs on `libsass`.
+
+First, you will need to install Node.js and Gulp; ample documentation exists
+online to guide you.
+
+To compile the Sass into updated CSS, from the command line, `cd` into the theme
+directory and run `npm install` to fetch all dependencies. Then, run `gulp` to
+start watching and compiling. The default gulp task will update documentation as
+necessary, as well as minify images and scripts, and auto-prefix CSS rulesets.
+
+## Version control
+
+TRF's entire codebase is version-controlled with git, hosted in a public 
+[GitHub account](https://github.com/rootwork/ringsforever).
+
+Do not change anything without committing it to git; even if it's a temporary
+or emergency fix, making one 5-second git commit will make it easier to track,
+change, or undo in the future.
+
+Because to date Exotica has had just one web developer, we go ahead and commit
+compiled CSS and minified JavaScript to the repository. Should we have multiple
+developers working together in the future, this approach could be revised.
+
+## Developer documentation
+
+Please review these sets of documentation before beginning development.
+
+**Please use comments in the Sass and JavaScript files, consistent with the
+documentation syntax, when adding new code.**
+
+Documentation is critical to keeping this theme maintainable. Please do not plan
+to "do it later". Do it now, as you write your code.
+
+### [Style Guide (SC5)](/sites/all/themes/trf/documentation/styleguide/)
+
+Documents the assorted designs, components, styles and markup patterns defined
+by the TRF site. You'll probably want to start here. Generated automatically
+using the default `gulp` task using comments in the Sass files.
+
+### [SassDoc](/sites/all/themes/trf/documentation/sassdoc/)
+
+Documents Sass breakpoints, functions and mixins, which are not included in a
+style guide. Generated automatically using the default `gulp` task using
+comments in the Sass files.
+
+### [JSDoc](/sites/all/themes/trf/documentation/jsdoc/)
+
+Documents custom JavaScript on the TRF site.
+
+### Coding standards
+
+TRF includes an [.editorconfig](http://editorconfig.org/) file; you should
+install the appropriate plugin for your text editor if necessary and these
+standards will be enforced automatically:
+
+* Indentation: 2 spaces, no tabs
+* Insert final new line: yes
+
+Additional standards for all files in the theme:
+
+* 80-character wide lines, with the exception of URLs or other strings that
+can't be broken across lines.
+
+These standards are specifically designed to align with the
+[Drupal coding standards](https://www.drupal.org/coding-standards).
+
+### Standards for writing Sass files
+
+* For class names, use SMACSS principles and BEM syntax, with styles broken into
+componentized Sass partials. Note this often means altering default Drupal
+templates or views to insert the appropriate class names.
+* Write in SCSS format ("CSS-style"), not Sass format.
+* Write rule sets across multiple lines, not all on a single line.
+* Insert a space between a property's colon and its value.
+* Insert a blank line between rule sets.
+* Comment heavily. Adhere to the KSS syntax for documenting styles
+automatically on builds, and SassDoc syntax for documenting breakpoints,
+functions and mixins.
+* Follow the Sass Inception Rule: Never nest more than four levels deep (and
+usually only one or two).
+* Strings such as URLs and font-families should be wrapped in 'single quotes'.
+* Numbers should be written with a preceding 0 if less than 1, e.g. `0.5`, for
+readability. The number 0 should never have units (such as px or em).
+* Mathematical operations in Sass should be wrapped in percentages for
+readability, e.g. `padding: (2 * $padding);`
+* In general, color rules should be referencing a Sass variable; if a color that
+does not have a Sass variable is being written in directly you should have a
+very good reason (not laziness).
+* When using multivalue rules, use a comma followed by a space, e.g.
+`color: rgba(0, 0, 0, 0.1);`
+* Many other conventions are followed from Hugo Giraudel's
+[Sass Guidelines](http://sass-guidelin.es/); when in doubt, follow the guideline
+there.
---
+++ sites/all/themes/trf/gulpfile.js
@@ -0,0 +1,258 @@
+var gulp = require("gulp");
+
+// Path variables
+var urlPath = 'http://www.titaniumringsforever.com';
+var themePath = '/sites/all/themes/trf/';
+var cssPath = 'css';
+var styleguidePath = 'documentation/styleguide';
+
+// Sass
+var sass = require("gulp-sass");
+var bulkSass = require('gulp-sass-bulk-import');
+
+// Preprocess plugins
+var autoprefixer = require('gulp-autoprefixer');
+var filter = require('gulp-filter')
+var sourcemaps = require('gulp-sourcemaps');
+
+// Images
+var images = require('gulp-imagemin');
+
+// Documentation
+var sassdoc = require('sassdoc');
+var jsdoc = require("gulp-jsdoc");
+var styleguide = require('sc5-styleguide');
+
+// Gulp plugins
+var cache = require('gulp-cache');
+var notify = require("gulp-notify");
+var shell = require('gulp-shell');
+var size = require('gulp-size');
+var rename = require('gulp-rename');
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+
+//
+// Compile Sass
+//
+gulp.task('sass', function () {
+  return gulp.src('sass/**/*.scss')
+
+  .pipe(bulkSass())
+
+  // Convert sass into css
+  .pipe(sass({
+    sourcemap: true,
+    sourceComments: 'normal',
+    onError: function(err) {
+      return notify().write(err);
+    }
+  }))
+
+  // Catch any SCSS errors and prevent them from crashing gulp
+  .on('error', function (error) {
+    console.error(error);
+    this.emit('end');
+  })
+
+  // Load existing internal sourcemap
+  .pipe(sourcemaps.init({loadMaps: true}))
+
+  // Autoprefix properties
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
+
+  // Write final .map file
+  .pipe(sourcemaps.write())
+
+  // Save the CSS
+  .pipe(gulp.dest(cssPath))
+
+  // Get file size (gzipped)
+  .pipe(size({
+    gzip: true
+  }))
+
+  // Notify to say the task has complete
+  .pipe(notify({
+    message: 'Sass compiling complete',
+    onLast: true
+  }))
+
+  // Filtering stream to only css files
+  .pipe(filter('sass/**/*.css'))
+
+  .pipe(browserSync.reload({stream:true}));
+});
+
+
+//
+// Compile JavaScript
+//
+gulp.task('js', function () {
+  return gulp.src('scripts/**/*.js')
+  .pipe(gulp.dest('scripts'))
+
+  // Notify to say the task has complete
+  .pipe(notify({
+    message: 'JavaScript compiling complete',
+    onLast: true
+  }))
+});
+
+
+//
+// Compress images
+//
+gulp.task('images', function() {
+  gulp.src('images/*.{gif,jpg,png}')
+
+  .pipe(cache(images({
+    optimizationLevel: 4,
+    progressive: true,
+    interlaced: true
+  })))
+
+  .pipe(gulp.dest('images/min/'))
+
+  // Notify to say the task has complete
+  .pipe(notify({
+    message: 'Image compression complete',
+    onLast: true
+  }))
+});
+
+
+//
+// Clear the theme registry with drush
+//
+gulp.task('drush', shell.task([
+  'drush cache-clear theme-registry'
+]));
+
+
+//
+// Run BrowserSync
+//
+gulp.task('browser-sync', function() {
+
+  // Watch files
+  var files = [
+    'styles/main.css',
+    'scripts/**/*.js',
+    'images/**/*',
+    'templates/**/*.php'
+  ];
+
+  // Initialize BrowserSync
+  browserSync.init(files, {
+    // BrowserSync with a php server
+    proxy: "trf.dev",
+    notify: true,
+    open: false
+  });
+});
+
+
+//
+// Compile JSDoc
+//
+// @todo Template syntax for gulp-jsdoc doesn't appear to work. Figure
+// out some mechanism for removing footer (and setting a better theme).
+// @see https://www.npmjs.com/package/gulp-jsdoc#template
+gulp.task('jsdoc', function () {
+  return gulp.src('scripts/*.js')
+
+  .pipe(jsdoc.parser({
+    version: '1.0',
+    template: {
+      path: 'ink-docstrap',
+      systemName: '',
+      footer: '',
+      copyright: '',
+      navType: 'vertical',
+      theme: 'cosmo',
+      linenums: true,
+      collapseSymbols: false,
+      inverseNav: false
+    }
+  }))
+
+  .pipe(jsdoc.generator('./documentation/jsdoc'))
+
+  // Notify to say the task has complete
+  .pipe(notify({
+    message: 'JSDoc update complete',
+    onLast: true
+  }))
+});
+
+
+//
+// Compile SassDoc
+//
+gulp.task('sassdoc', function () {
+  return gulp.src('sass/**/*.scss')
+
+  .pipe(sassdoc({
+    dest: './documentation/sassdoc'
+  }))
+
+  // Notify to say the task has complete
+  .pipe(notify({
+    message: 'SassDoc update complete',
+    onLast: true
+  }))
+});
+
+
+//
+// Compile SC5 Styleguide
+//
+gulp.task('styleguide:generate', function() {
+  return gulp.src(['sass/**/*.scss'])
+
+  .pipe(styleguide.generate({
+      title: 'Exotica Style Guide: TRF theme',
+      server: false,
+      rootPath: styleguidePath,
+      appRoot: themePath + styleguidePath,
+      overviewPath: 'documentation/styleguide.md'
+    }))
+
+  .pipe(gulp.dest(styleguidePath))
+});
+
+gulp.task('styleguide:applystyles', function() {
+  return gulp.src([cssPath + '/main.css'])
+
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+
+    .pipe(styleguide.applyStyles())
+
+    .pipe(gulp.dest(styleguidePath))
+
+  // Notify to say the task has complete
+  .pipe(notify({
+    message: 'Styleguide update complete',
+    onLast: true
+  }))
+});
+
+gulp.task('styleguide-watch', ['styleguide'], function() {
+  gulp.watch(['sass/**/*.scss'], ['styleguide']);
+});
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+
+//
+// Default task to be run with `gulp`
+//
+gulp.task('default', ['sassdoc', 'styleguide', 'sass', 'jsdoc', 'js', 'images', 'browser-sync'], function () {
+  gulp.watch("sass/**/*.scss", ['sassdoc', 'styleguide', 'sass']);
+  gulp.watch("scripts/**/*.js", ['jsdoc', 'js']);
+  gulp.watch('images/*' , ['images']);
+});
