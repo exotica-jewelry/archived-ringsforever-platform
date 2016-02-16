d95513718ef00eba8151f19a154db9a0b2014c92
Ivan Boothe <ivan@rootwork.org>
2016-02-15 16:27:53 -0800

    Moved function replacing taxonomy page titles with 'display title' from template.php to trf_ring_taxonomies feature

--- sites/all/themes/trf/template.php
+++ sites/all/themes/trf/template.php
@@ -25,25 +25,3 @@ function trf_form_contact_site_form_alter(&$form, &$form_state) {
   $site_name = $GLOBALS['conf']['site_name'];
   drupal_set_title('Contact ' . $site_name);
 }
-
-/**
- * Implements hook_preprocess_views_view().
- *
- * Overrides commerce_kickstart_taxonomy to set "title to display" as the
- * collection title in the view header.
- *
- * Also removes collection_title_url variable as we've removed the image that
- * would be used as the background.
- */
-function trf_preprocess_views_view(&$vars) {
-  $view = $vars['view'];
-  if ($view->name == 'collection_products') {
-    if ($view->current_display == 'page') {
-      // Keep the previous theming.
-      $vars['classes_array'][] = 'view-collection-taxonomy-term';
-      $tid = $view->args['0'];
-      $term = taxonomy_term_load($tid);
-      $vars['collection_title'] = $term->field_cat_title['und'][0]['safe_value'];
-    }
-  }
-}
