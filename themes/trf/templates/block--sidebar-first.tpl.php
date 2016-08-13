f49024d089e6a8ade55288e7787bab9d8d3e7b9a
Ivan Boothe <ivan@rootwork.org>
2016-08-13 16:58:37 -0700

    Adding block template override for first sidebar

---
+++ themes/trf/templates/block--sidebar-first.tpl.php
@@ -0,0 +1,18 @@
+<?php /* /Overrides profiles/commerce_kickstart/themes/contrib/omega/omega/templates/block.tpl.php */ ?>
+<?php $tag = $block->subject ? 'section' : 'div'; ?>
+<<?php print $tag; ?><?php print $attributes; ?>>
+  <div class="block-inner clearfix">
+    <?php
+      $block_handle = '<h3 class="block-title">' . $block->subject . '</h3>';
+
+      print theme(
+        'ctools_collapsible',
+        array(
+          'handle' => $block_handle,
+          'content' => $content,
+          'collapsed' => TRUE
+        )
+      );
+    ?>
+  </div>
+</<?php print $tag; ?>>
