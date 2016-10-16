f44af5f45f03ae004396e9773e3de5cfe3ff3099
Ivan Boothe <ivan@rootwork.org>
2016-10-16 13:41:27 -0700

    Replacing JS imagezoom wrapper formatting with an actual template

--- themes/trf/js/trf.js
+++ themes/trf/js/trf.js
@@ -9,30 +9,6 @@ if ('addEventListener' in document) {
 // Scripts to add once the page has loaded.
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Add captions to images with zoom set, based on their title attribute.
-  function addRingCaptions() {
-    var images = document.querySelectorAll('.imagezoom-image'),
-    L = images.length,
-    fig = document.createElement('figure'),
-    who,
-    temp
-    while(L) {
-      temp = fig.cloneNode(false);
-      who = images[--L];
-      caption = who.getAttribute("title");
-      if (caption) {
-        who.parentNode.insertBefore(temp, who);
-        content = document.createElement('figcaption');
-        content.className = 'image__caption';
-        content.innerHTML = caption;
-        temp.appendChild(who);
-        temp.appendChild(content);
-      }
-    }
-  };
-
-  addRingCaptions();
-
 /**
  * The jQuery plugin namespace.
  * @external "jQuery.fn"
